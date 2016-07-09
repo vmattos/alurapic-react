84bc48cd7f2c0d983104121c460095ea4f075431
Vitor Mattos <vitor.mattos@vtex.com.br>
2016-07-09 13:42:05 -0300

    Uses webpack

--- alurapic-react/stage3/config/express.js
+++ alurapic-react/stage3/config/express.js
@@ -1,11 +1,15 @@
 var express = require('express')
     ,app = express()
     ,bodyParser = require('body-parser')
+    ,webpack = require('webpack')
+    ,webpackMiddleware = require('webpack-dev-middleware')
+    ,webpackConfig = require('./webpack.config.dev')
     ,routes = require('../app/routes');
 
 app.use(express.static('./public'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+app.use(webpackMiddleware(webpack(webpackConfig)));
 
 routes(app);
 

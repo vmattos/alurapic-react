ab6495b6d6b03f05fd620a5840845cedb0f7c2db
Vitor Mattos <vitor.mattos@vtex.com.br>
2016-07-14 07:40:57 -0300

    Uses browser history

--- alurapic-react/stage5/public/components/App/index.js
+++ alurapic-react/stage5/public/components/App/index.js
@@ -1,14 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { Router, Route, hashHistory } from 'react-router'
+import { Router, Route, IndexRedirect, browserHistory } from 'react-router'
 
 import PictureListPage from '../../pages/PictureListPage'
 
 class App extends React.Component {
   render() {
     return (
-      <Router history={hashHistory}>
-        <Route path="/" component={PictureListPage} />
+      <Router history={browserHistory}>
+        <Route path="/">
+          <IndexRedirect to="/fotos" />
+          <Route path="fotos" component={PictureListPage} />
+        </Route>
       </Router>
     );
   }
